5dec972dc7bb1209d1797e5a275c8903f0859056
ckr <ckr3453@gmail.com>
2023-03-21 02:03:39 +0900

    Fix: EnableJpaAuditing 적용

--- src/main/java/com/kakao/SearchApplication.java
+++ src/main/java/com/kakao/SearchApplication.java
@@ -2,12 +2,13 @@ package com.kakao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SearchApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SearchApplication.class, args);
 	}
-
 }

b2c82433f87a9cd66e56edec97a2d5261cc46bd0
ckr <ckr3453@gmail.com>
2023-03-22 03:12:02 +0900

    Fix: 미사용 코드 삭제

--- src/main/java/com/kakao/exception/ErrorCode.java
+++ src/main/java/com/kakao/exception/ErrorCode.java
@@ -16,7 +16,6 @@ public enum ErrorCode {
 
     INVALID_ARGUMENT("매개변수가 유효하지 않습니다."),
     ARGUMENT_TYPE_MISMATCH( "매개변수의 타입이 올바르지 않습니다."),
-    ARGUMENT_NOT_VALID("매개변수가 유효하지 않습니다."),
     NOT_FOUND("존재하지 않는 api 입니다."),
     INTERNAL_SERVER_ERROR("서버 오류가 발생했습니다. 관리자에게 문의해주세요."),
     METHOD_NOT_ALLOWED("지원하지 않는 http method 입니다.");

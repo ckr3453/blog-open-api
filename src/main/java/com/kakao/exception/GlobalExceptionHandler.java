4652e138f06e788c580cacb822daf4c2a365159b
ckr <ckr3453@gmail.com>
2023-03-22 02:16:46 +0900

    Fix: bindexception 등 추가

--- src/main/java/com/kakao/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kakao/exception/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package com.kakao.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -21,47 +23,46 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 @RestControllerAdvice(basePackages = "com.kakao")
 public class GlobalExceptionHandler {
 
-	@ExceptionHandler(Exception.class)
-	public ResponseEntity<ErrorResponse> defaultExceptionHandler(Exception e) {
-		String message = "\n=*=*=*=    Exception     =*=*=*=\n";
-		log.error(message + e.getMessage(), e);
-		return new ResponseEntity<>(ErrorResponse.builder()
-            .errorMessage("INTERNAL_SERVER_ERROR")
-            .description("서버 오류가 발생하였습니다. 관리자에게 문의하세요.")
-            .build(),
-            HttpStatus.INTERNAL_SERVER_ERROR);
-	}
+    @ExceptionHandler(BindException.class)
+    protected ResponseEntity<ErrorResponse> bindExceptionHandler(BindException e) {
+        String message = "\n=*=*=*=     BindException     =*=*=*=\n";
+        log.error(message + e.getMessage(), e);
+        return new ResponseEntity<>(ErrorResponse.of(ErrorCode.INVALID_ARGUMENT, e.getBindingResult().getFieldErrors().get(0).getDefaultMessage()), HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public ResponseEntity<ErrorResponse> MethodArgumentTypeMismatchExceptionHandler(MethodArgumentTypeMismatchException e) {
+    protected ResponseEntity<ErrorResponse> methodArgumentTypeMismatchExceptionHandler(MethodArgumentTypeMismatchException e) {
         String message = "\n=*=*=*=     MethodArgumentTypeMismatchException     =*=*=*=\n";
         log.error(message + e.getMessage(), e);
-        return new ResponseEntity<>(ErrorResponse.builder()
-            .errorMessage("ILLEGAL_ARGUMENT")
-            .description("매개변수의 타입이 올바르지 않습니다. (" + e.getCause() + ")")
-            .build(),
-            HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(ErrorResponse.of(ErrorCode.ARGUMENT_TYPE_MISMATCH), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> MethodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
+    protected ResponseEntity<ErrorResponse> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
         String message = "\n=*=*=*=     MethodArgumentNotValidException     =*=*=*=\n";
         log.error(message + e.getMessage(), e);
-        return new ResponseEntity<>(ErrorResponse.builder()
-            .errorMessage("ILLEGAL_ARGUMENT")
-            .description("매개변수의 검증이 실패하였습니다. (" + e.getCause() + ")")
-            .build(),
-            HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(ErrorResponse.of(ErrorCode.ARGUMENT_NOT_VALID, e.getBindingResult().getFieldErrors().get(0).getDefaultMessage()), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(NoHandlerFoundException.class)
-    public ResponseEntity<ErrorResponse> NoHandlerFoundExceptionHandler(NoHandlerFoundException e) {
+    protected ResponseEntity<ErrorResponse> noHandlerFoundExceptionHandler(NoHandlerFoundException e) {
         String message = "\n=*=*=*=     NoHandlerFoundException     =*=*=*=\n";
         log.error(message + e.getMessage(), e);
-        return new ResponseEntity<>(ErrorResponse.builder()
-            .errorMessage("URL_NOT_FOUND")
-            .description("존재하지 않는 url 입니다.")
-            .build(),
-            HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(ErrorResponse.of(ErrorCode.NOT_FOUND), HttpStatus.NOT_FOUND);
+
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    protected ResponseEntity<ErrorResponse> httpRequestMethodNotSupportedExceptionHandler(HttpRequestMethodNotSupportedException e) {
+        String message = "\n=*=*=*=     HttpRequestMethodNotSupportedException     =*=*=*=\n";
+        log.error(message + e.getMessage(), e);
+        return new ResponseEntity<>(ErrorResponse.of(ErrorCode.METHOD_NOT_ALLOWED), HttpStatus.METHOD_NOT_ALLOWED);
+    }
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<ErrorResponse> defaultExceptionHandler(Exception e) {
+        String message = "\n=*=*=*=    Exception     =*=*=*=\n";
+        log.error(message + e.getMessage(), e);
+        return new ResponseEntity<>(ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }

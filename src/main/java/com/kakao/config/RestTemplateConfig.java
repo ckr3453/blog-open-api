539124633d98150306efd43e1c8d298703be64be
ckr <ckr3453@gmail.com>
2023-03-22 02:17:35 +0900

    Fix: 미사용 import 삭제

--- src/main/java/com/kakao/config/RestTemplateConfig.java
+++ src/main/java/com/kakao/config/RestTemplateConfig.java
@@ -5,7 +5,6 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 /**

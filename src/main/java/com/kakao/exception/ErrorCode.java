8b25cc418dda0160da66d0d273934829c89526a7
ckr <ckr3453@gmail.com>
2023-03-22 02:15:43 +0900

    Fix: 예외처리용 error code 추가

--- src/main/java/com/kakao/exception/ErrorCode.java
+++ src/main/java/com/kakao/exception/ErrorCode.java
@@ -1,5 +1,8 @@
 package com.kakao.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 /**
  * packageName : com.kakao.exception
  * fileName    : ErrorCode
@@ -7,5 +10,16 @@ package com.kakao.exception;
  * date        : 2023/03/19
  * description :
  */
+@Getter
+@AllArgsConstructor
 public enum ErrorCode {
+
+    INVALID_ARGUMENT("매개변수가 유효하지 않습니다."),
+    ARGUMENT_TYPE_MISMATCH( "매개변수의 타입이 올바르지 않습니다."),
+    ARGUMENT_NOT_VALID("매개변수가 유효하지 않습니다."),
+    NOT_FOUND("존재하지 않는 api 입니다."),
+    INTERNAL_SERVER_ERROR("서버 오류가 발생했습니다. 관리자에게 문의해주세요."),
+    METHOD_NOT_ALLOWED("지원하지 않는 http method 입니다.");
+
+    private final String msg;
 }
